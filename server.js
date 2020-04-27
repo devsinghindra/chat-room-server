ff4191f0a0156998addf64c3cbfb10336ac5b1bc
Devendra Singh <devsinghindra@gmail.com>
2020-04-27 18:49:10 +0530

    added chat for users

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
+
 const PORT = process.env.PORT || 5000;
 
 const routes = require("./router");
@@ -11,7 +13,34 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", function (socket) {
-  console.log("We have a new connection");
+  // console.log("We have a new connection");
+  socket.on("join", function ({ name, room }, callback) {
+    const { error, user } = addUser(socket.id, name, room);
+    if (error) {
+      return callback(error);
+    }
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
+
   socket.on("disconnect", function () {
     console.log("User has left!");
   });
